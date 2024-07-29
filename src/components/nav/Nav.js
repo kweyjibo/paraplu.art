6e8e5d1a01aa8b3095cab8b103696a8258fda152
Alla Dolzhenko <kweyjiba12@gmail.com>
2024-07-29 20:10:30 +0200

    Navigation: +icons

--- src/components/nav/Nav.js
+++ src/components/nav/Nav.js
@@ -1,4 +1,5 @@
 import { NavLink } from "react-router-dom";
+import navIcons from "../../img/icons/nav.svg";
 
 export default function Nav() {
   return (
@@ -6,16 +7,25 @@ export default function Nav() {
       <ul className="nav-list">
         <li className="nav-i">
           <NavLink to="/" className="nav-lk">
+            <svg className="nav-svg" width={24} height={24}>
+              <use href={`${navIcons}#home`} />
+            </svg>
             Home
           </NavLink>
         </li>
         <li className="nav-i">
           <NavLink to="/about" className="nav-lk">
+            <svg className="nav-svg" width={24} height={24}>
+              <use href={`${navIcons}#about`} />
+            </svg>
             About
           </NavLink>
         </li>
         <li className="nav-i">
           <NavLink to="/rules" className="nav-lk">
+            <svg className="nav-svg" width={24} height={24}>
+              <use href={`${navIcons}#rules`} />
+            </svg>
             Rules
           </NavLink>
         </li>
