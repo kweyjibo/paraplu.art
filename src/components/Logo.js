9f1cbb4b80559e9f004c3ba3be33644f006ec7fd
Alla Dolzhenko <kweyjiba12@gmail.com>
2024-08-12 21:23:46 +0200

    Structure of project

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,10 +1,20 @@
+import { Link } from "react-router-dom";
+
 import { ReactComponent as Logopicture } from "./../img/paraplu-logo.svg";
+import { ReactComponent as Logopicturemobile } from "./../img/m/paraplu-logo.svg";
 
 function Logo() {
   return (
     <>
-      <div className="logo">
-        <Logopicture alt="Logo" width={224} />
+      <div className="logo __desktop">
+        <Link to="/">
+          <Logopicture alt="Logo" width={224} />
+        </Link>
+      </div>
+      <div className="logo __mobile">
+        <Link to="/">
+          <Logopicturemobile alt="Logo" width={84} />
+        </Link>
       </div>
     </>
   );
