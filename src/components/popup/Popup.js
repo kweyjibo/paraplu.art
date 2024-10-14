8ddcb0d9cd1e3d3eec9cba22f64a74ecbe4e8e6a
Alla Dolzhenko <kweyjiba12@gmail.com>
2024-10-14 12:29:18 +0200

    ParapluArt: custom hook useOutsideClick

--- src/components/popup/Popup.js
+++ src/components/popup/Popup.js
@@ -1,11 +1,30 @@
 import Icon from "../icon/Icon";
 import Button from "../Button";
 import socialsIcons from "../../img/icons/socials.svg";
+import { useOutsideClick } from "../../hooks/useOutsideClick";
+import { useState } from "react";
 
 function Popup({ id, typeIcon, children, addClass }) {
+  const [isChecked, setIsChecked] = useState(false);
+
+  function handleClose() {
+    setIsChecked(false);
+  }
+
+  const ref = useOutsideClick(handleClose);
+
+  const handleCheckboxChange = () => {
+    setIsChecked(!isChecked);
+  };
+
   return (
-    <div className="popup">
-      <input type="checkbox" id={id} />
+    <div className="popup" ref={ref}>
+      <input
+        type="checkbox"
+        id={id}
+        checked={isChecked}
+        onChange={handleCheckboxChange}
+      />
       <span className="popup-clickable-area">
         <label htmlFor={id} className="open-popup-btn"></label>
         <Button type="circle">
