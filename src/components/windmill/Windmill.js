dd4b44b8bccce176136fe7b84010d63d8cbd7561
Alla Dolzhenko <kweyjiba12@gmail.com>
2024-11-05 12:05:23 +0100

    ParapluArt: decoration Windmill

---
+++ src/components/windmill/Windmill.js
@@ -0,0 +1,11 @@
+import { ReactComponent as WindmillAnimation } from "./../../img/windmill.svg";
+
+function Windmill() {
+  return (
+    <div className="windmill-cnt">
+      <WindmillAnimation alt="Windmill" width={194} />
+    </div>
+  );
+}
+
+export default Windmill;
