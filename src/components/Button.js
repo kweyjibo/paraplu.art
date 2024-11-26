14c5e48510239864c50e5dc5f066b50f6049df23
Alla Dolzhenko <kweyjiba12@gmail.com>
2024-11-26 10:07:21 +0100

    ParapluArt: Button

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,9 +1,10 @@
 import classNames from "classnames";
 
-function Button({ children, onClick, type, size }) {
+function Button({ children, onClick, type, size, additionalClass }) {
   const buttonClass = classNames("btn", {
     [type]: type,
     [size]: size,
+    [additionalClass]: additionalClass,
   });
   return (
     <button className={buttonClass} onClick={onClick}>
