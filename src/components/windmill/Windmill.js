214cb7022eca49b5bdfe49319481c55d42276d77
Alla Dolzhenko <kweyjiba12@gmail.com>
2025-02-26 08:45:09 +0100

    Paraplu.art: Windmill animation

--- src/components/windmill/Windmill.js
+++ src/components/windmill/Windmill.js
@@ -1,9 +1,15 @@
-import { ReactComponent as WindmillAnimation } from "./../../img/windmill.svg";
+import { ReactComponent as WindmillBody } from "./../../img/windmill-body.svg";
+import { ReactComponent as WindmillAnimation } from "./../../img/windmill-fl.svg";
 
 function Windmill() {
   return (
-    <div className="windmill-cnt">
-      <WindmillAnimation alt="Windmill" width={194} />
+    <div className="windmill">
+      <div className="windmill-cnt">
+        <WindmillBody alt="Windmill" width={97} />
+        <div className="windmill-fl">
+          <WindmillAnimation alt="Windmill" width={193} />
+        </div>
+      </div>
     </div>
   );
 }
