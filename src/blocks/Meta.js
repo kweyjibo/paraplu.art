af01546f60d4756fad4a92c270bf630a97936493
Alla Dolzhenko <kweyjiba12@gmail.com>
2025-03-07 10:03:34 +0100

    Paraplu.art: Meta data fix

--- src/blocks/Meta.js
+++ src/blocks/Meta.js
@@ -1,28 +1,31 @@
-import { Helmet } from "react-helmet";
+import { Helmet } from "react-helmet-async";
 
 function Meta() {
   return (
-    <div>
-      <Helmet>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <Helmet>
+      <meta charSet="utf-8" />
+      <meta name="viewport" content="width=device-width,initial-scale=1" />
 
-        <title>Paraplu.art - all about dreams</title>
-        <meta
-          name="description"
-          content="Paraplu.art - dreams about something good. What dreams may come."
-        />
-        <meta name="theme-color" content="#ffffff" />
+      <title>Paraplu.art</title>
+      <meta
+        name="description"
+        content="Paraplu.art - dreams about something good. Digital illustration and simple art about life, nature, and people."
+      />
+      <meta
+        name="keywords"
+        content="illustration,digital art,digital illustration,funny cards,funny illustrations"
+      />
+      <meta name="theme-color" content="#ffffff" />
 
-        <meta property="og:type" content="website" />
-        <meta property="og:title" content="Paraplu.art" />
-        <meta
-          property="og:description"
-          content="Paraplu.art - dreams about something good. What dreams may come."
-        />
-        <meta property="og:url" content="https://paraplu.art" />
-      </Helmet>
-    </div>
+      <meta property="og:type" content="website" />
+      <meta property="og:title" content="Paraplu.art" />
+      <meta
+        property="og:description"
+        content="Paraplu.art - dreams about something good. Digital illustration and simple art about life, nature, and people."
+      />
+      <meta property="og:url" content="https://paraplu.art" />
+      <meta property="og:image" content="img/logo.png"></meta>
+    </Helmet>
   );
 }
 
