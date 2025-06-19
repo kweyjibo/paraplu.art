3810a4930b78fd1ad0dbf45070b7b922707fcd80
Alla Dolzhenko <kweyjiba12@gmail.com>
2025-06-19 12:03:18 +0200

    Paraplu.art: Error component ++

--- src/components/error/Error.js
+++ src/components/error/Error.js
@@ -1,7 +1,18 @@
+import errorImg from "../../img/error/error.jpg";
+
 function Error({ message }) {
   return (
-    <div>
-      <p>&#9940; {message}</p>
+    <div className="error-cnt">
+      <h1 className="error-title">Oops!</h1>
+
+      <section className="error-section">
+        <figure className="error-ifigure">
+          <img src={errorImg} alt="Error" className="error-img" />
+        </figure>
+
+        <h2 className="error-subtitle">{message}</h2>
+        <p></p>
+      </section>
     </div>
   );
 }
