63df97ac1fd0548f8a01191f5524fe0b0214c247
Alla Dolzhenko <kweyjiba12@gmail.com>
2025-09-10 10:57:24 +0200

    Paraplu.art: footer update

--- src/blocks/Footer.js
+++ src/blocks/Footer.js
@@ -3,49 +3,51 @@ import { EMAIL, BLOG, TELEGRAM, IG, COPYRIGHT, ABOUT } from "./footerContacts";
 export default function Footer() {
   return (
     <footer className="footer cell4">
-      <div className="footer__section __1">
-        <h3 className="footer__title">Links</h3>
+      <div className="footer-cnt">
+        <div className="footer__section __1">
+          <h3 className="footer__title">Links</h3>
 
-        <ul className="footer__list">
-          <li>
-            <a href={BLOG} className="footer__link">
-              Personal blog
-            </a>
-          </li>
-          <li>
-            <a href={TELEGRAM} className="footer__link">
-              Telegram
-            </a>
-          </li>
-          <li>
-            <a href={IG} className="footer__link">
-              Instagram
-            </a>
-          </li>
-        </ul>
-      </div>
-      <div className="footer__section __2">
-        <h3 className="footer__title">E-mail</h3>
-        <a href={`mailto:${EMAIL}`} className="footer__link">
-          {EMAIL}
-        </a>
-      </div>
-      <div className="footer__section __3">
-        <p>
-          Handcrafted by{" "}
-          <a href={ABOUT} className="footer__link">
-            me
+          <ul className="footer__list">
+            <li>
+              <a href={BLOG} className="footer__link">
+                Personal blog
+              </a>
+            </li>
+            <li>
+              <a href={TELEGRAM} className="footer__link">
+                Telegram
+              </a>
+            </li>
+            <li>
+              <a href={IG} className="footer__link">
+                Instagram
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div className="footer__section __2">
+          <h3 className="footer__title">E-mail</h3>
+          <a href={`mailto:${EMAIL}`} className="footer__link __email">
+            {EMAIL}
           </a>
-        </p>
-        <p>
-          Designed by{" "}
-          <a href={ABOUT} className="footer__link">
-            me
-          </a>
-        </p>
-      </div>
-      <div className="footer__section __4">
-        <span className="footer-copyright">&copy; {COPYRIGHT}</span>
+        </div>
+        <div className="footer__section __3">
+          <p>
+            Handcrafted by{" "}
+            <a href={ABOUT} className="footer__link">
+              me
+            </a>
+          </p>
+          <p>
+            Designed by{" "}
+            <a href={ABOUT} className="footer__link">
+              me
+            </a>
+          </p>
+        </div>
+        <div className="footer__section __4">
+          <span className="footer-copyright">&copy; {COPYRIGHT}</span>
+        </div>
       </div>
     </footer>
   );
---
+++ src/blocks/footerContacts.js
@@ -0,0 +1,6 @@
+export const EMAIL = "kweyjibo@gmail.com";
+export const BLOG = "https://kweyjibo.com";
+export const TELEGRAM = "https://t.me/parapluart";
+export const IG = "https://www.instagram.com/paraplu.art/";
+export const COPYRIGHT = "Paraplu.art";
+export const ABOUT = "https://kweyjibo.com/about";
